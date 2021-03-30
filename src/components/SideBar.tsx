10485b65dd0aadf3f1307a1bcbbf9b35ed0c720e
Leonardo Marcos <leonardo.marcos@cwi.com.br>
2021-03-30 20:34:35 -0300

    complete complementary challenge

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,38 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from "./Button";
+
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface Props {
+  genres: Array<GenreResponseProps>
+  setSelectedGenreId: (id: number) => void;
+  selectedGenreId: number;
+}
+
+export function SideBar({ genres, setSelectedGenreId, selectedGenreId }: Props) {
+  
+  function handleClickButton(id: number) {
+    setSelectedGenreId(id);
+  }
+
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+    </nav>
+  )
 }
